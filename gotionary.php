7383817dde0245b039f2652e18505bc76446acf7
Adam Franco <afranco@middlebury.edu>
2010-09-02 15:43:25 -0400

    Fixed case-sensitivity mismatch and dtd url in the doctype declarations.
    Can now use validation tools to verify output correctness.

--- gotionary.php
+++ gotionary.php
@@ -9,7 +9,7 @@ if (isset($_GET["letter"]) && preg_match("/^[A-Za-z]|\[0-9\]$/", $_GET["letter"]
 }
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title>The GOtionary</title>
--- header.php
+++ header.php
@@ -20,7 +20,7 @@ if (isset($_SESSION["AUTH"])) {
 }
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title>The GOtrol Panel</title>
