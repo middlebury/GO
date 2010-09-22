0deca22480f171e906f81093c2f1b860a256647b
Matt La France <lafrance@middlebury.edu>
2010-09-22 16:44:35 -0400

    Adjusted js/jquery for the edit interface
    - Now hidden inputs are generated that will pass an array of
    admins and aliases when the form is subbmitted
    - Working on deleting these hidden inputs when delete is selected.
    This has been problematic. In progress.

--- update2.js
+++ update2.js
@@ -1,128 +1,78 @@
 
 $(document).ready(function(){
+	alert('hi');
+	addRemoveBehavior('#alias_list','#add_alias_button','#add_alias_text');
+	addRemoveBehavior('#admin_list','#add_admin_button','#add_admin_text');
 	
-	// Aliases
-	
-	// Add class to each li which is an index of the list element
-	$("#alias_list li").each(function(index){
-  	$(this).attr("class", index)
-	});
-	
-	// Add a class to each "Delete" button which is an index of the li
-	$("#alias_list li>input").each(function(index){
-  	$(this).attr("class", index)
-	});
-	
-	// Add a function to each Delete button that removes the
-	// <li> with the corresponding class/index
-	$("#alias_list li>input").each(function(index){
-		$(this).bind("click", function(){
-			//alert('clicked');
-			var our_class = "." + index;
-			//alert(index);
-  		$("#alias_list li").remove(our_class);
-		})
-	});
-	
-	// Add a function to the "Add Alias" button
-	$("#add_alias_button").bind("click", function(){
-		//alert('clicked');
-		var alias = $("#add_alias_text").val();
-		var invalid_alias = 0;
-		
-		// If the alias is already in the list then it's invalid
-		$("#alias_list li").each(function(){
-			//for(var i = 0; i > $(this).text().length; i++) {
-				if (alias == $(this).text().trim()) {
-					invalid_alias = 1;
-				}
-			//}
+	function addRemoveBehavior(ul_id, button_id, text_id) {
+		// Add class to each li which is an index of the list element
+		$(ul_id + " li").each(function(index){
+  		$(this).attr("class", index);
+  		//also make a hiden input for each
+  		// IE7 does not support .trim()
+  		if ($.browser.msie) {
+  			$("<input type='hidden' name='" + ul_id.slice(1) + "[]' value='" + $(obj).text().replace(/(^[\s\xA0]+|[\s\xA0]+$)/g, '') + "' />").prependTo('form');
+				// other browsers do
+			} else {
+				$("<input type='hidden' name='" + ul_id.slice(1) + "[]' value='" + $(this).text().trim() + "' />").prependTo('form');
+			}
 		});
-		
-		// If the alias is valid then add the new alias to the list
-		if (alias.length > 0 & invalid_alias != 1) {
-			//alert($("#add_alias_text").val());
-			$("<li>"+$("#add_alias_text").val()+" <input type='button' value='Delete' /></li>").appendTo("#alias_list");
-		}
-		
-		// Give the new alias (and all others) a class corresponding to the index (again)
-		$("#alias_list li").each(function(index){
-    	$(this).attr("class", index)
-		})
-		
-		// Give the new alias' Delete button (and all others) a function that will delete the
-		// corresponding li (again)
-		$("#alias_list li>input").each(function(index){
+	
+		// Add a class to each "Delete" button which is an index of the li
+		$(ul_id + " li>input").each(function(index){
+  		$(this).attr("class", index)
+		});
+	
+		// Add a function to each Delete button that removes the
+		// <li> with the corresponding class/index
+		$(ul_id + " li>input").each(function(index){
 			$(this).bind("click", function(){
-				//alert('clicked');
 				var our_class = "." + index;
-				//alert(index);
-  			$("#alias_list li").remove(our_class);
+  			$(ul_id + " li").remove(our_class);
 			})
 		});
-	});
 	
-	// Admins
-	
-	// Add class to each li which is an index of the list element
-	$("#admin_list li").each(function(index){
-  	$(this).attr("class", index)
-	});
-	
-	// Add a class to each "Delete" button which is an index of the li
-	$("#aadmin_list li>input").each(function(index){
-  	$(this).attr("class", index)
-	});
-	
-	// Add a function to each Delete button that removes the
-	// <li> with the corresponding class/index
-	$("#admin_list li>input").each(function(index){
-		$(this).bind("click", function(){
-			//alert('clicked');
-			var our_class = "." + index;
-			//alert(index);
-  		$("#admin_list li").remove(our_class);
-		})
-	});
-	
-	// Add a function to the "Add Admin" button
-	
-	$("#add_admin_button").bind("click", function(){
-		//alert('clicked');
-		var admin = $("#add_admin_text").val();
-		var invalid_admin = 0;
+		// Add a function to the "Add" button
+		$(button_id).bind("click", function(){
+			var value = $(text_id).val();
+			var invalid_value = 0;
 		
-		// If the alias is already in the list then it's invalid
-		$("#admin_list li").each(function(){
-			//for(var i = 0; i > $(this).text().length; i++) {
-				if (admin == $(this).text().trim()) {
-					invalid_admin = 1;
+			// If the value is already in the list then it's invalid
+			$(ul_id + " li").each(function(){
+				// IE 7 doesn't support .trim()
+				if ($.browser.msie) {
+					if (value == $(this).text().replace(/(^[\s\xA0]+|[\s\xA0]+$)/g, '')) {
+						invalid_value = 1;
+					}
+				// other browsers do
+				} else {
+					if (value == $(this).text().trim()) {
+						invalid_value = 1;
+					}
 				}
-			//}
-		});
+			});
 		
-		// If the admin is valid then add the new alias to the list
-		if (admin.length > 0 & invalid_admin != 1) {
-			//alert($("#add_alias_text").val());
-			$("<li>"+$("#add_admin_text").val()+" <input type='button' value='Delete' /></li>").appendTo("#admin_list");
-		}
+			// If the value is valid then add the new value to the list
+			if (value.length > 0 & invalid_value != 1) {
+				$("<li>" + $(text_id).val() + " <input type='button' value='Delete' /></li>").appendTo(ul_id);
+				//and create a hidden input
+				$("<input type='hidden' name='" + ul_id.slice(1) + "[]' value='" + $(text_id).val() + "' />").prependTo('form');
+			}
 		
-		// Give the new admin (and all others) a class corresponding to the index (again)
-		$("#admin_list li").each(function(index){
-    	$(this).attr("class", index)
-		})
-		
-		// Give the new admin's Delete button (and all others) a function that will delete the
-		// corresponding li (again)
-		$("#admin_list li>input").each(function(index){
-			$(this).bind("click", function(){
-				//alert('clicked');
-				var our_class = "." + index;
-				//alert(index);
-  			$("#admin_list li").remove(our_class);
+			// Give the new value (and all others) a class corresponding to the index (again)
+			$(ul_id + " li").each(function(index){
+    		$(this).attr("class", index);
 			})
+		
+			// Give the new value's Delete button (and all others) a function that will delete the
+			// corresponding li (again)
+			$(ul_id + " li>input").each(function(index){
+				$(this).bind("click", function(){
+					var our_class = "." + index;
+  				$(ul_id + " li").remove(our_class);
+				})
+			});
 		});
-	});
-	
+	}
 	
 });
