ee31159871c488a47a2e23d06d74a8131751a85e
Adam Franco <afranco@middlebury.edu>
2017-09-14 07:18:38 -0400

    Do not force error reporting in production.

--- go_functions.php
+++ go_functions.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
 //copy pasted function to get IP address via client ip or x
 //forwarded first before falling back on remote addr
 function getRealIpAddr() {
