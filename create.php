d2fde89e9b3b9c9dfd60056b3c3a9c7360502b4c
Ian McBride <imcbride@middlebury.edu>
2021-08-24 11:01:15 -0400

    Remove the protocol from the path to the jquery include on certain pages so that it works over HTTPS.

--- create.php
+++ create.php
@@ -8,7 +8,7 @@ require_once "admin_nav.php";
 ?>
 
 <!-- Include jQuery/JS to apply add remove behavior to the admin and alias lists -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script src="addremove.js" type="text/javascript"></script>
 <script src="md5.js" type="text/javascript"></script>
 
